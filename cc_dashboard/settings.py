8457209a433834860f1b8a30a8c8037087bf1785
magg666 <mw23127@gmail.com>
2020-01-15 08:16:56 +0100

    initial commit

--- cc_dashboard/settings.py
+++ cc_dashboard/settings.py
@@ -145,6 +145,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'frontend', "build", "static"),
---
+++ static/precache-manifest.ab6867298a05c71804970eb9fcb72404.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c450c17355e838d5338c843a92013f80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f2bc0a8b98771685a4e",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "3263b66ba28df5b81537",
+    "url": "/static/css/main.866e9a0f.chunk.css"
+  },
+  {
+    "revision": "0f2bc0a8b98771685a4e",
+    "url": "/static/js/2.cf4fe43e.chunk.js"
+  },
+  {
+    "revision": "09f741da76051e47d05cdb4aaaa78610",
+    "url": "/static/js/2.cf4fe43e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3263b66ba28df5b81537",
+    "url": "/static/js/main.9a03921d.chunk.js"
+  },
+  {
+    "revision": "3da6c20433ae3bc29b36",
+    "url": "/static/js/runtime-main.20953027.js"
+  },
+  {
+    "revision": "e7c6c02635467313d6bbe021e2005a21",
+    "url": "/static/media/codecool-logo.e7c6c026.png"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
