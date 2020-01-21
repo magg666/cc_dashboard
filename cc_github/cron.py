0e5429fe553dd6e7064a6d3a5aacb81a90faa607
magg666 <mw23127@gmail.com>
2020-01-21 11:38:34 +0100

    initial commit

--- cc_github/cron.py
+++ cc_github/cron.py
@@ -23,21 +23,22 @@ class GetWeeklyStatistic(CronJobBase):
         repositories_data = get_github_statistics()
         last_week_index = -1
         for repository in repositories_data:
-            for data in repository:
+            if repository:
+                for data in repository:
 
-                contributor = data['author']['login']
-                repository_id = data['repository_id']
-                week = date.fromtimestamp(data['weeks'][last_week_index]['w'])
-                additions = data['weeks'][last_week_index]['a']
-                deletions = data['weeks'][last_week_index]['d']
-                commits = data['weeks'][last_week_index]['c']
+                    contributor = data['author']['login']
+                    repository_id = data['repository_id']
+                    week = date.fromtimestamp(data['weeks'][last_week_index]['w'])
+                    additions = data['weeks'][last_week_index]['a']
+                    deletions = data['weeks'][last_week_index]['d']
+                    commits = data['weeks'][last_week_index]['c']
 
-                WeeklyStatistic.objects.update_or_create(repository_id=repository_id,
-                                                         contributor=contributor,
-                                                         week=week,
-                                                         defaults={"commits": commits,
-                                                                   "additions": additions,
-                                                                   'deletions': deletions})
+                    WeeklyStatistic.objects.update_or_create(repository_id=repository_id,
+                                                             contributor=contributor,
+                                                             week=week,
+                                                             defaults={"commits": commits,
+                                                                       "additions": additions,
+                                                                       'deletions': deletions})
 
 
 class GetTotalStatistic(CronJobBase):
@@ -58,17 +59,18 @@ class GetTotalStatistic(CronJobBase):
         repositories_data = get_github_statistics()
 
         for repository in repositories_data:
-            commits = sum(data['total'] for data in repository)
-            additions = 0
-            deletions = 0
+            if repository:
+                commits = sum(data['total'] for data in repository)
+                additions = 0
+                deletions = 0
 
-            for data in repository:
-                repository_id = data['repository_id']
-                weeks = data['weeks']
-                additions += sum(item['a'] for item in weeks)
-                deletions += sum(item['d'] for item in weeks)
+                for data in repository:
+                    repository_id = data['repository_id']
+                    weeks = data['weeks']
+                    additions += sum(item['a'] for item in weeks)
+                    deletions += sum(item['d'] for item in weeks)
 
-                TotalStatistic.objects.update_or_create(repository_id=repository_id,
-                                                        defaults={'commits': commits,
-                                                                  'additions': additions,
-                                                                  'deletions': deletions})
+                    TotalStatistic.objects.update_or_create(repository_id=repository_id,
+                                                            defaults={'commits': commits,
+                                                                      'additions': additions,
+                                                                      'deletions': deletions})
--- cc_github/utils.py
+++ cc_github/utils.py
@@ -28,22 +28,21 @@ def get_github_statistics() -> str or list:
     processed_data = []
 
     for repository in active_repositories:
-        if repository:
-            owner = repository.owner
-            project = repository.project
+        owner = repository.owner
+        project = repository.project
 
-            url = f'https://api.github.com/repos/{owner}/{project}/stats/contributors'
-            try:
-                response = requests.get(url, headers=head)
-                if not response.status_code // 100 == 2:
-                    return f"Error: Unexpected response {response}"
+        url = f'https://api.github.com/repos/{owner}/{project}/stats/contributors'
+        try:
+            response = requests.get(url, headers=head)
+            if not response.status_code // 100 == 2:
+                return f"Error: Unexpected response {response}"
 
-                repository_statistic = response.json()
-                for data in repository_statistic:
-                    data['repository_id'] = repository.id
-                processed_data.append(repository_statistic)
+            repository_statistic = response.json()
+            for data in repository_statistic:
+                data['repository_id'] = repository.id
+            processed_data.append(repository_statistic)
 
-            except requests.exceptions.RequestException as e:
-                return f"Serious error: {e}"
+        except requests.exceptions.RequestException as e:
+            return f"Serious error: {e}"
 
     return processed_data
